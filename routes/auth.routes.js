98120b7a076fadcfd3bb71576150d8bf6df99dec
juli.jimenez <juli.jimenez@duocuc.cl>
2022-09-05 11:19:31 -0300

    Validando login

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,13 +1,18 @@
 const { Router } = require("express");
 const { check } = require("express-validator");
 const { login } = require("../controller/auth.controller");
+const { validarCampos } = require("../middlewares/validar-campos");
 
 const router = Router();
 
-router.post("/login", login),
+router.post(
+  "/login",
   [
     check("email", "El correo es obligatorio").isEmail(),
     check("password", "La contrasena es obligatoria").not().isEmpty(),
-  ];
+    validarCampos,
+  ],
+  login
+);
 
 module.exports = router;
