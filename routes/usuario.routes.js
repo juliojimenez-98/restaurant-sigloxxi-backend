b0f5dc6d52fb0c87f3abbe185c3c735b54c86651
juli.jimenez <juli.jimenez@duocuc.cl>
2022-09-05 11:36:10 -0300

    Validando campos de crear usuarios

--- routes/usuario.routes.js
+++ routes/usuario.routes.js
@@ -1,14 +1,26 @@
 const { Router } = require("express");
+const { check } = require("express-validator");
 const {
   crearUsuario,
   obtenerUsuarios,
   obtenerRoles,
 } = require("../controller/usuario.controller");
+const { validarCampos } = require("../middlewares/validar-campos");
 
 const router = Router();
 
 //Crear usuario
-router.post("/usuario", crearUsuario);
+router.post(
+  "/usuario",
+  [
+    check("email", "El correo es obligatorio").isEmail(),
+    check("nombre", "El nombre es obligatorio").not().isEmpty(),
+    check("appa", "El apellido es obligatorio").not().isEmpty(),
+    check("id_rol", "El rol es obligatorio").not().isEmpty(),
+    validarCampos,
+  ],
+  crearUsuario
+);
 //Obtener todos los usuarios
 router.get("/usuarios", obtenerUsuarios);
 router.get("/roles", obtenerRoles);
