2fa3ba40b5964e53d32e5765874f1609bd428b08
julio jimenez <juli.jimenez@duocucl.cl>
2022-11-02 16:38:58 -0300

    fix

--- models/pedido_ing.js
+++ models/pedido_ing.js
@@ -1,50 +1,54 @@
-const {DataTypes} = require("sequelize");
+const { DataTypes } = require("sequelize");
 const dbConnection = require("../database/config");
 const Ingredientes = require("./ingredientes");
 const Proveedor = require("./proveedor");
 
 const Pedido_ing = dbConnection.dbConnection.define(
-    "pedido_ing",
-    {
-        id_pedido: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-        },
-        fecha_despacho:{
-            type:DataTypes.DATE,
-            allowNull: false,
-        },
-        cantidad:{
-            type: DataTypes.FLOAT,
-            allowNull: false,
-        },
-        precio:{
-            type: DataTypes.INTEGER,
-            allowNull: false,
-        },
-        id_proveedor:{
-            type: DataTypes.INTEGER,
-            references:{
-                model: "Proveedor",
-                key: "id_proveedor",
-            },
-            allowNull: false,
-        },
-        id_ing:{
-            type: DataTypes.INTEGER,
-            references:{
-                model:"Ingredientes",
-                key:"id_ing",
-            },
-            allowNull: false,            
-        },
+  "pedido_ing",
+  {
+    id_pedido: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
     },
-    {
-        freezeTableName: true,
-    }
+    fecha_despacho: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    cantidad: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+    },
+    precio: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    estado: {
+      type: DataTypes.INTEGER,
+      defaultValue: 1,
+    },
+    id_proveedor: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "Proveedor",
+        key: "id_proveedor",
+      },
+      allowNull: false,
+    },
+    id_ing: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "Ingredientes",
+        key: "id_ing",
+      },
+      allowNull: false,
+    },
+  },
+  {
+    freezeTableName: true,
+  }
 );
 
-Pedido_ing.hasOne(Proveedor, {foreignKey: "id_proveedor"});
-Pedido_ing.hasOne(Ingredientes, {foreignKey: "id_ing"});
+Pedido_ing.hasOne(Proveedor, { foreignKey: "id_proveedor" });
+Pedido_ing.hasOne(Ingredientes, { foreignKey: "id_ing" });
 
 module.exports = Pedido_ing;
