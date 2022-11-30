dd44de2c5f6ac2b36dc189afa7dd556484ed4ec7
julio jimenez <juli.jimenez@duocucl.cl>
2022-11-30 19:19:54 -0300

    Fix pedido plato, validacion reserva form

--- routes/reserva.routes.js
+++ routes/reserva.routes.js
@@ -14,7 +14,22 @@ const router = Router();
 //
 router.get("/reservas", obtenerReservas);
 //Crear reserva
-router.post("/reserva", crearReserva);
+router.post(
+  "/reserva",
+  [
+    check("hora_reserva", "La hora de la reserva es obligatoria")
+      .not()
+      .isEmpty(),
+    check("cant_personas", "La cantidad de personas es obligatoria")
+      .not()
+      .isEmpty(),
+    check("fecha_reserva", "La fecha de la reserva es obligatoria")
+      .not()
+      .isEmpty(),
+    validarCampos,
+  ],
+  crearReserva
+);
 
 router.get("/reserva/:email", obtenerReservaPorCliente);
 
