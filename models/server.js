191260cc5972633dc97085d46500bea59a90d5d8
Julio Jimenez <juli.jimenez@alumnos.duoc.cl>
2022-12-01 01:09:26 -0300

    fix

--- models/server.js
+++ models/server.js
@@ -7,7 +7,7 @@ const fileUpload = require("express-fileupload");
 class Server {
   constructor() {
     this.app = express();
-    this.port = process.env.PORT||"/.netlify/.functions";
+    this.port = process.env.PORT;
 
     this.authPath = "/api";
     this.buscar = "/api/buscar";
